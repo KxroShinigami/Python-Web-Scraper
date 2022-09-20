4e9aba12daa1ede57277dd05267d1ea0b4603d4b
KxroShinigami <90672361+KxroShinigami@users.noreply.github.com>
2022-09-20 18:21:44 +0200

    1st Step

--- web-scraper.py
+++ web-scraper.py
@@ -1,17 +1,42 @@
 #Python Web Sraber by KxroShinigami
 
 #ToDo: pysimplegui, time countdown
+# https://realpython.com/pysimplegui-python/
+# https://www.geeksforgeeks.org/how-to-create-a-countdown-timer-using-python/, https://realpython.com/python-sleep/
 
-
+# +--+--+--+--+--+--+
+#    Imports
 
 import requests
 
-# Making a GET request
-r = requests.get('www.google.com')
 
-# check status code for response received
-# success code - 200
-print(r)
+# +--+--+--+--+--+--+
+#    Input
 
-# print content of request
-print(r.content)
+print("Please enter a valid Website URL:")
+print("Example: 'https://www.google.com'")
+Website = input()
+
+# +--+--+--+--+--+--+
+#    Get-Request Website
+
+WebsiteGetRequest = requests.get(Website)
+
+
+# +--+--+--+--+--+--+
+#    Status Code Website
+
+# 1xxs – Informational responses
+# 2xxs – Success
+# 3xxs – Redirection
+# 4xxs – Client errors: Page not found.
+# 5xxs – Server errors: Failure.
+
+StatusCode = str(WebsiteGetRequest.status_code)
+
+if(StatusCode == "200"):
+    Status =  "Online"
+elif(StatusCode == "404"):
+    Status = "Not Found"
+
+print("Your Website is:", Status)
