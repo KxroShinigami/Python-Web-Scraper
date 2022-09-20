acb0009c342c1161ef4c496d84083f8308be0c40
KxroShinigami <90672361+KxroShinigami@users.noreply.github.com>
2022-09-20 21:22:41 +0200

    first Steps

--- web-scraper.py
+++ web-scraper.py
@@ -8,6 +8,7 @@
 #    Imports
 
 import requests
+from bs4 import BeautifulSoup
 
 
 # +--+--+--+--+--+--+
@@ -16,6 +17,10 @@ import requests
 print("Please enter a valid Website URL:")
 print("Example: 'https://www.google.com'")
 Website = input()
+
+#TEST
+Website = "https://www.mediamarkt.de/de/campaign/angebote-computer-buero#root"
+
 
 # +--+--+--+--+--+--+
 #    Get-Request Website
@@ -36,7 +41,55 @@ StatusCode = str(WebsiteGetRequest.status_code)
 
 if(StatusCode == "200"):
     Status =  "Online"
+elif(StatusCode == "403"):
+    Status = "Forbidden"
 elif(StatusCode == "404"):
     Status = "Not Found"
+elif(StatusCode == "410"):
+    Status = "Gone"
+elif(StatusCode == "500" or StatusCode == "503"):
+    Status = "Unavailable"
+else:
+    Status = "Weird"
 
-print("Your Website is:", Status)
+print("Your Website is:", Status, "(", StatusCode, ")\n\n")
+
+
+# +--+--+--+--+--+--+
+#    Parsing the HTML
+
+soup = BeautifulSoup(WebsiteGetRequest.content, 'html.parser')
+
+
+# +--+--+--+--+--+--+
+#    Display some information
+
+print(soup.title)
+
+
+# +--+--+--+--+--+--+
+#    Extraction of Content
+
+print("\n\nYour Content is: \n\n")
+
+s = soup.find('p', class_='BaseTypo-sc-1jga2g7-0 izkVco StyledInfoTypo-sc-1jga2g7-1 doYUxh')
+
+lines = s
+ 
+for line in lines:
+    print(line.text)
+
+
+# Informationen MediaMarkt Produkt-Namen
+# class="BaseTypo-sc-1jga2g7-0 izkVco StyledInfoTypo-sc-1jga2g7-1 doYUxh"
+
+# Informationen MediaMarkt Preise
+# class="StyledUnbrandedPriceDisplayWrapper-sc-1n9i68m-0 gWEOl"
+    # "ScreenreaderTextSpan-sc-11hj9ix-0 kZCfsu"
+
+
+
+
+
+# +--+--+--+--+--+--+
+#    Searching for relevant Information
